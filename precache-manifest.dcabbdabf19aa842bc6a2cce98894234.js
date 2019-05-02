7b97a40e3e973c0599b65e0bde8164f9f66d9793
Pankaj Singh <pankaj_arunsingh@ymail.com>
2019-05-03 09:10:18 +1000

    Updates

---
+++ precache-manifest.dcabbdabf19aa842bc6a2cce98894234.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bbf8f8312d3f88cf00ca7d4c9f0177d",
+    "url": "/flappybutterfly/index.html"
+  },
+  {
+    "revision": "80a029ad3989609aed74",
+    "url": "/flappybutterfly/static/css/main.3e545749.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/flappybutterfly/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "80a029ad3989609aed74",
+    "url": "/flappybutterfly/static/js/main.a90f3923.chunk.js"
+  },
+  {
+    "revision": "0e66293b7861d9dfbc8f",
+    "url": "/flappybutterfly/static/js/runtime~main.ef5ed3f5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/flappybutterfly/static/media/logo.5d5d9eef.svg"
+  }
+]);
